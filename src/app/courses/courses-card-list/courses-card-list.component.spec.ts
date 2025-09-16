c2c609b5cafeb31b418529bacbf4981c19c487a2
Andrew Shin <andrewshin@Andrews-MacBook-Pro.local>
2025-09-16 07:56:11 -0700

    committed what I have

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 import { CoursesCardListComponent } from "./courses-card-list.component";
 import { CoursesModule } from "../courses.module";
 import { COURSES } from "../../../../server/db-data";
@@ -9,14 +9,23 @@ import { Course } from "../model/course";
 import { setupCourses } from "../common/setup-test-data";
 
 describe("CoursesCardListComponent", () => {
-  beforeEach(() => {
+  let component: CoursesCardListComponent;
+  let fixture: ComponentFixture<CoursesCardListComponent>;
+
+  // fixure is a utility for testing components
+
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      // declarations: [],
       imports: [CoursesModule],
-    });
-  });
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(CoursesCardListComponent);
+        component = fixture.componentInstance;
+      });
+  }));
   it("should create the component", () => {
-    pending();
+    expect(component).toBeDefined();
   });
 
   it("should display the course list", () => {
